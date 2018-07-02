e893ef36a7f0ca7316002fb65f44b64dc5cac317
snehalthakare <snehal.thakare@wohlig.com>
2018-07-02 18:28:13 +0530

    chnages for header

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,19 +1,53 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-        name: "Home",
-        classis: "active",
-        anchor: "home",
-        subnav: [{
-            name: "Subnav1",
+            name: "Home",
             classis: "active",
-            anchor: "home"
-        }]
-    }, {
-        name: "Links",
-        classis: "active",
-        anchor: "links",
-        subnav: []
-    }];
+            anchor: "home",
+            subnav: [
+                //     {
+                //     name: "Subnav1",
+                //     classis: "active",
+                //     anchor: "home"
+                // }
+            ]
+        },
+        {
+            name: "About Us",
+            classis: "active",
+            anchor: "Aboutus",
+            subnav: []
+        },
+        {
+            name: "Services",
+            classis: "active",
+            anchor: "services",
+            subnav: []
+        },
+        {
+            name: "Partners",
+            classis: "active",
+            anchor: "partners",
+            subnav: []
+        },
+        {
+            name: "Insights",
+            classis: "active",
+            anchor: "insights",
+            subnav: []
+        },
+        {
+            name: "Contact Us",
+            classis: "active",
+            anchor: "contact",
+            subnav: []
+        },
+        //  {
+        //     name: "Links",
+        //     classis: "active",
+        //     anchor: "links",
+        //     subnav: []
+        // }
+    ];
 
     return {
         getNavigation: function () {
