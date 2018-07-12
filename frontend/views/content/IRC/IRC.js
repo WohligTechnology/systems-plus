ba3f9c1e9605dec706764837467b4585da9d4dab
snehalthakare <snehal.thakare@wohlig.com>
2018-07-12 15:31:01 +0530

    changes for home page

--- frontend/views/content/IRC/IRC.js
+++ frontend/views/content/IRC/IRC.js
@@ -1,6 +1,13 @@
-myApp.controller('IRCCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
+myApp.controller('IRCCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http, $uibModal) {
     $scope.template = TemplateService.getHTML("content/IRC/IRC.html");
     TemplateService.title = "IRC"; // This is the Title of the Website
     $scope.navigation = NavigationService.getNavigation();
-
+    $scope.openWhitepaper = function () {
+        $uibModal.open({
+            animation: true,
+            templateUrl: "views/modal/whitepaper.html",
+            scope: $scope,
+            size: 'md'
+        });
+    };
 });
